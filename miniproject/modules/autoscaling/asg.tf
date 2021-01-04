84e55e50c5196ba207b4366e1254cbcd41b34b69
Serhii Vasylenko <serhii.vasylenko@grammarly.com>
2021-01-05 01:02:18 +0200

    miniproject update

--- miniproject/modules/autoscaling/asg.tf
+++ miniproject/modules/autoscaling/asg.tf
@@ -69,7 +69,7 @@ resource "aws_autoscaling_group" "this" {
     version = "$Latest"
   }
   health_check_type         = "ELB" //change to ELB on the next class
-  health_check_grace_period = 45
+  health_check_grace_period = 120
   vpc_zone_identifier       = var.subnet_ids_list
 }
 
